0a91e398bb768b1760ce70f2939c319c9b4e98d7
adepp12 <madeputrautama262@gmail.com>
2026-01-23 15:17:15 +0800

    add: specify the columns to be cleaned, round Total AUM data

--- function/preprocessing.py
+++ function/preprocessing.py
@@ -3,12 +3,18 @@ def cleaning_data(raw_df):
     '''
     documentation
     '''
-    df = raw_df
+    df = raw_df.copy()
+
+    columns_to_clean = ["Last NAV", "Return 1Y", "Drawdown 1Y", "Expense Ratio", "Min. Pembelian"]
 
     # cleaning persen (%) and comma (,) 
-    for col in df.columns:
-        df[col] = df[col].str.replace('%', '')
-        df[col] = df[col].str.replace(',', '')
+    for column in columns_to_clean:
+        df[column] = df[column].str.replace('%', '')
+        df[column] = df[column].str.replace(',', '')
+    
+    df['Expense Ratio'] = df['Expense Ratio'].str.replace('-', '0')
+    df['Total AUM'] = df['Total AUM'].str.replace('Rp', '')
+    df['Min. Pembelian'] = df['Min. Pembelian'].str.replace('IDR ', '')
 
     # change the format of the “Total AUM” column to Billion (B)
     df["isTrillion"] = 0
@@ -20,10 +26,11 @@ def cleaning_data(raw_df):
         else:
             df.loc[i, "Total AUM"] = row['Total AUM'].replace('B', '')
 
-    df.loc[df["isTrillion"] == 1, "Total AUM"] = df.loc[df["isTrillion"] == 1, "Total AUM"].astype(float) * 1000
+    df.loc[df["isTrillion"] == 1, "Total AUM"] = (
+        df.loc[df["isTrillion"] == 1, "Total AUM"].astype(float) * 1000).round()
 
     # convert data type to float
-    float_columns = ['Return', 'Drawdown', 'Total AUM', 'Last NAV']
+    float_columns = ['Return 1Y', 'Drawdown 1Y', 'Total AUM', 'Last NAV', 'Expense Ratio']
 
     for col in float_columns:
         df[col] = df[col].astype(float)
