793f0509fcc8722b3f96a1c3fa6357fb22e4f745
adepp12 <madeputrautama262@gmail.com>
2026-01-28 23:04:41 +0800

    change: get the criteria data from constant.py

--- pages/list_criteria.py
+++ pages/list_criteria.py
@@ -2,50 +2,12 @@ import streamlit as st
 import pandas as pd
 from streamlit_extras.great_tables import great_tables
 from great_tables import GT
+from constant import DATA_CRITERIA
 
 st.title("Daftar Kriteria Seleksi")
 st.write("Berikut merupakan daftar kriteria beserta penjelasannya yang digunakan untuk pertimbangan dalam memilih produk reksa dana")
 
-data_criteria = [
-    {
-        "Nama Kriteria": "Return",
-        "Deskripsi": "Total keuntungan yang diperoleh reksa dana",
-        "Satuan": "Persen (%)",
-        "Jenis": "🟢 Benefit"
-    },
-    {
-        "Nama Kriteria": "Drawdown",
-        "Deskripsi": "Penurunan terbesar yang pernah terjadi pada reksa dana dalam periode waktu tertentu",
-        "Satuan": "Persen (%)",
-        "Jenis": "🟢 Benefit"
-    },
-    {
-        "Nama Kriteria": "Expense Ratio",
-        "Deskripsi": "Biaya pengelolaan reksa dana oleh Manajer Investasi, mencakup biaya transaksi, biaya manajemen, biaya kustodian, dan lainnya",
-        "Satuan": "Persen (%)",
-        "Jenis": "🔴 Cost"
-    },
-    {
-        "Nama Kriteria": "Asset Under Management (AUM)",
-        "Deskripsi": "Total dana yang dikelola oleh Manajer Investasi pada suatu reksa dana",
-        "Satuan": "Miliar (M) / Triliun (T) Rupiah",
-        "Jenis": "🟢 Benefit"
-    },
-    {
-        "Nama Kriteria": "Net Asset Value (NAV)",
-        "Deskripsi": "Nilai pasar atau harga bersih reksa dana saat ini yang dihitung dan diperbarui setiap hari kerja. Kenaikan NAV mencerminkan potensi keuntungan dari reksa dana tersebut",
-        "Satuan": "Ribu Rupiah",
-        "Jenis": "🟢 Benefit"
-    },
-    {
-        "Nama Kriteria": "Minimal Pembelian",
-        "Deskripsi": "Jumlah minimal nilai pembelian reksa dana pada satu kali transaksi",
-        "Satuan": "Ribu Rupiah",
-        "Jenis": "🔴 Cost"
-    }
-]
-
-df_criteria = pd.DataFrame(data_criteria)
+df_criteria = pd.DataFrame(DATA_CRITERIA).drop(columns=["Nama Kolom"])
 
 table_criteria = (
     GT(df_criteria)
@@ -76,4 +38,12 @@ st.html(
 </style>
 """)
 
-great_tables(table_criteria)
+great_tables(table_criteria)  
+
+# menggunakan markdown table, lebih cepat
+# md_table = "| **Nama Kriteria** | **Deskripsi** | **Satuan** | **Jenis** |\n"
+# md_table += "| --- | --- | --- | --- |\n"
+# for row in DATA_CRITERIA:
+#     md_table += f"| {row['Nama Kriteria']} | {row['Deskripsi']} | {row['Satuan']} | {row['Jenis']} |\n"
+
+# st.markdown(md_table)
