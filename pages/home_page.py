cd6cbe4161e990c0c71e4f9d195429606611d0ad
adepp12 <madeputrautama262@gmail.com>
2026-01-28 21:54:12 +0800

    add: article references for the DSS method

--- pages/home_page.py
+++ pages/home_page.py
@@ -7,5 +7,5 @@ Sistem Pendukung Keputusan (SPK) ini merupakan sebuah sistem yang dapat membantu
 
 Alternatif yang tersedia merupakan produk reksa dana saham yang terdapat pada aplikasi investasi **Bibit**, yang dapat diakses pada laman [Daftar Reksa Dana Bibit](https://bibit.id/reksadana?limit=60&page=1&sort=asc&sort_by=7&tradable=1&type=1).  
 
-Metode SPK yang digunakan pada sistem ini yaitu *Rank Order Centroid (ROC)*[[1]](https://journals.ums.ac.id/index.php/emitor/article/view/1) untuk menentukan bobot kriteria dan *Multi-Objective Optimization on the basis of Ratio Analysis (MOORA)*[[2]](https://journals.ums.ac.id/index.php/emitor/article/view/1) untuk metode pemeringkatan akhir alternatif.
+Metode SPK yang digunakan pada sistem ini yaitu *Rank Order Centroid (ROC)*[[1]](https://ejournal.sidyanusa.org/index.php/jkdn/article/view/193), [[2]](https://jtiik.ub.ac.id/index.php/jtiik/article/view/9218) untuk menentukan bobot kriteria dan *Multi-Objective Optimization on the basis of Ratio Analysis (MOORA)*[[3]](https://ejournal.undiksha.ac.id/index.php/insert/article/view/59054), [[4]](https://journal.ilmudata.co.id/index.php/RIGGS/article/view/780) untuk metode pemeringkatan akhir alternatif.
 ''')
