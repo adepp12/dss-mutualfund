28d087e056bda6f66e57cbf8a1f1bac58a048bed
adepp12 <madeputrautama262@gmail.com>
2026-01-29 10:57:24 +0800

    delete: URL_DATASET

---
+++ constant.py
@@ -0,0 +1,44 @@
+DATA_CRITERIA = [
+    {
+        "Nama Kriteria": "Return",
+        "Deskripsi": "Total keuntungan yang diperoleh reksa dana",
+        "Satuan": "Persen (%)",
+        "Jenis": "🟢 Benefit",
+        "Nama Kolom": "Return 1Y"
+    },
+    {
+        "Nama Kriteria": "Drawdown",
+        "Deskripsi": "Penurunan terbesar yang pernah terjadi pada reksa dana dalam periode waktu tertentu",
+        "Satuan": "Persen (%)",
+        "Jenis": "🟢 Benefit",
+        "Nama Kolom": "Drawdown 1Y"
+    },
+    {
+        "Nama Kriteria": "Expense Ratio",
+        "Deskripsi": "Biaya pengelolaan reksa dana oleh Manajer Investasi, mencakup biaya transaksi, biaya manajemen, biaya kustodian, dan lainnya",
+        "Satuan": "Persen (%)",
+        "Jenis": "🔴 Cost",
+        "Nama Kolom": "Expense Ratio"
+    },
+    {
+        "Nama Kriteria": "Asset Under Management (AUM)",
+        "Deskripsi": "Total dana yang dikelola oleh Manajer Investasi pada suatu reksa dana",
+        "Satuan": "Miliar (M) / Triliun (T) Rupiah",
+        "Jenis": "🟢 Benefit",
+        "Nama Kolom": "Total AUM"
+    },
+    {
+        "Nama Kriteria": "Net Asset Value (NAV)",
+        "Deskripsi": "Nilai pasar atau harga bersih reksa dana saat ini yang dihitung dan diperbarui setiap hari kerja. Kenaikan NAV mencerminkan potensi keuntungan dari reksa dana tersebut",
+        "Satuan": "Ribu Rupiah",
+        "Jenis": "🟢 Benefit",
+        "Nama Kolom": "Last NAV"
+    },
+    {
+        "Nama Kriteria": "Minimal Pembelian",
+        "Deskripsi": "Jumlah minimal nilai pembelian reksa dana pada satu kali transaksi",
+        "Satuan": "Ribu Rupiah",
+        "Jenis": "🔴 Cost",
+        "Nama Kolom": "Min. Pembelian"
+    }
+]
